55be714fb746e0e24be01d3b61c578ee93b94e33
cheerfulchangmin <chuchangmin@gmail.com>
2021-01-25 13:44:09 -0500

    testing branch Add fav food

--- 1.1_Github_VCS.py
+++ 1.1_Github_VCS.py
@@ -250,6 +250,7 @@ Tell me some interesting facts about yourself.
 ------------------Student 25------------------
 This is the testing1 Branch
 Leo
+Hokkien mee
 
 Marc Hermon
 I have four children ages 10-27.
